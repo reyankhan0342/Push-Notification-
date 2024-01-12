f2fd37e5428ee54fe58d033ba546e94694026dfb
reyankhan0342 <sashaandoo0342@gmail.com>
2024-01-12 11:35:03 +0500

    firebase push notification done using firebase api key

---
+++ CarRentAdminPanel/lib/screens/splash_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.green
+      ),
+    );
+  }
+}
---
+++ get_x_course/lib/getx/get _X_routing/getx_routing.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class GetXRouting extends StatelessWidget {
+  const GetXRouting({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        title: Text('Get X Routing',style: TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+          fontWeight: FontWeight.w800,
+        ),),
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
